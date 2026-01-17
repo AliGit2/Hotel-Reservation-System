e5b44b57d253c1c2a2d0fb173a08729637e7575b
Syed Ali Raza <ar719701@gmail.com>
2026-01-18 01:32:32 +0500

    HotelChain test added

---
+++ HotelChainTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.Date;
+
+public class HotelChainTest {
+
+    @Test
+    public void testCreateReserverPayer() {
+        // Arrange
+        HotelChain chain = new HotelChain("Serena Group");
+        String payerId = "P-99";
+        String card = "9999-8888";
+
+        // Act
+        chain.createReserverPayer(payerId, card);
+
+        // Assert
+        assertNotNull("Chain should not be null", chain);
+    }
+
+    @Test
+    public void testAddHotel() {
+        // Arrange
+        HotelChain chain = new HotelChain("Lux Hotel");
+        Hotel hotel = new Hotel("Lux Karachi");
+
+        // Act
+        chain.addHotel(hotel);
+
+        // Assert
+        assertNotNull("Hotel should be added to chain", chain);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidChainName() {
+        // Arrange & Act (Defensive Programming Check)
+        new HotelChain(""); 
+    }
+}
